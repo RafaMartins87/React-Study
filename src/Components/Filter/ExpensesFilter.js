3e02f968ebbd31a1e0278ea8679b64dd6e597b94
rafael martins <rafael.martins87@outlook.com>
2022-07-06 13:09:58 -0300

    fix(06-07): pegando valores selecionados no filtro ano

--- src/Components/Filter/ExpensesFilter.js
+++ src/Components/Filter/ExpensesFilter.js
@@ -7,8 +7,7 @@ const ExpensesFilter = (props) => {
   const [selectInput, setSelectInput] = useState('');
   
   const expensesFilterHandler = (event)=>{
-    setSelectInput(event.target.value); 
-    console.log(event.target.value);
+    setSelectInput(event.target.value);
     props.onFilterChange(event.target.value);
   };  
 
@@ -17,7 +16,7 @@ const ExpensesFilter = (props) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select onChange={expensesFilterHandler}>
-          <option value={selectInput}>2022</option>
+          <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
           <option value='2019'>2019</option>
