33b5d3889baad468681c55be629401e7fa64e3e8
rafael martins <rafael.martins87@outlook.com>
2022-07-06 13:28:29 -0300

    ajustando state

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -2,15 +2,19 @@ import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import '../Filter/ExpensesFilter.css';
 import ExpensesFilter from '../Filter/ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+    const [selectInput, setSelectInput] = useState('2020');
+
     const showSelectChange = (selectInput)=>{
         console.log('ta no expenses: ' + selectInput);
+        setSelectInput(selectInput);
     };
 
     return (        
         <div>
-            <ExpensesFilter onFilterChange={showSelectChange}/>
+            <ExpensesFilter selected={selectInput} onChangeFilter={showSelectChange}/>
             <Card className="expenses">
                 <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
--- src/Components/Filter/ExpensesFilter.js
+++ src/Components/Filter/ExpensesFilter.js
@@ -4,18 +4,16 @@ import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
 
-  const [selectInput, setSelectInput] = useState('');
-  
   const expensesFilterHandler = (event)=>{
-    setSelectInput(event.target.value);
-    props.onFilterChange(event.target.value);
+    console.log('filter: ' + event.target.value);
+    props.onChangeFilter(event.target.value);
   };  
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={expensesFilterHandler}>
+        <select value={props.selected} onChange={expensesFilterHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
